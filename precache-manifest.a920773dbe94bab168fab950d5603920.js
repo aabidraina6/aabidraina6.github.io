f5267eea06ceb72dae25d13d497a4e530919d116
aabidraina6 <aabidraina5@gmail.com>
2023-01-26 03:12:23 +0530

    new version

---
+++ precache-manifest.a920773dbe94bab168fab950d5603920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b81876096e0361c65e26",
+    "url": "/static/js/main.b8187609.chunk.js"
+  },
+  {
+    "revision": "5dcf03f4d5830863315b",
+    "url": "/static/css/1.55f48ad9.chunk.css"
+  },
+  {
+    "revision": "5dcf03f4d5830863315b",
+    "url": "/static/js/1.5dcf03f4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3816f86e4125e6f30a090bfb12227bbb",
+    "url": "/index.html"
+  }
+];
